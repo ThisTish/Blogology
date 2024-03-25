230507d86010db9cb4d2f43f56a2d52a83e7d1cc
tishthegoat <tishthegoat@gmail.com>
2024-03-25 17:30:38 -0600

    worked on populating blog, not working

--- assets/JS/blog.js
+++ assets/JS/blog.js
@@ -1,16 +1,35 @@
 // const card = document.querySelector('#blogs')
 
 
-const storedForm = JSON.parse(localStorage.getItem('form'));
-// console.log(storedForm);
+const blogCards = JSON.parse(localStorage.getItem('form'));
+console.log(blogCards);
 
-const makeBlog = function(blogCard){
-	const blogList = document.querySelector('#blogs');
-	const newBlog = document.createElement('li');
+const fillBlog = function(blogCards){
 	
-	const header = document.createElement('h4');
-	header.textContent = storedForm.title;
-	newBlog.appendChild(header);
+	const blogList = document.querySelector('#blogs');
+	console.log(blogList);
+
+	blogCards.forEach(blogCard =>{
+		const newBlog = document.createElement('li');
+			
+		const header = document.createElement('h4');
+		header.textContent = blogCard.Title;
+		
+		const blogContent = document.createElement('p');
+		blogContent.textContent = blogCard.Content;
+		
+		const username = document.createElement('footer')
+		username.textContent = blogCard.Username;
+		
+		blogList.appendChild(newBlog)
+		blogList.appendChild(header);
+		newBlog.appendChild(blogContent);
+		newBlog.appendChild(username);
+		
+		blogList.appendChild(newBlog);
+		console.log(blogCard)
+	})
+	fillBlog(blogCards);
 	
 
 
--- assets/JS/form.js
+++ assets/JS/form.js
@@ -7,14 +7,18 @@ const postBtn = document.querySelector('#postBtn');
 
 const tempArray = [];
 
-form.addEventListener('submit', (filledForm) => {
-	filledForm.preventDefault();
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
 	const makeBlog = new FormData(form);
-	const blog = Object.fromEntries(makeBlog);
-	console.log(blog);
-
-	const string = JSON.stringify(blog);
+	const blog = {};
+	makeBlog.forEach((value,key) => {
+		blog[key] = value;
+	});
+	tempArray.push(blog);
+	console.log(tempArray);
+	const string = JSON.stringify(tempArray);
 	localStorage.setItem('form', string);
+	
 
 // ? change to actual url?
 	window.location.href = 'blog.html';
