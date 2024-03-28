3ddf99329addaca64faaf5f1409bc9da438064ba
tishthegoat <tishthegoat@gmail.com>
2024-03-27 19:56:17 -0600

    added darkMode function to toggle button

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -1,1 +1,36 @@
 // todo light/dark toggle
+
+let darkMode = localStorage.getItem('darkMode');
+const toggleBtn = document.querySelector('#toggleBtn');
+
+
+// todo walk through and match up with html & css after styling.
+
+
+const turnDarkMode = () =>{
+
+document.body.classList.add('darkMode');
+
+localStorage.setItem('darkMode', 'enabled')
+}
+
+const turnLightMode = () => {
+
+	document.body.classList.remove('darkMode');
+	
+	localStorage.setItem('darkMode', 'disabled');
+};
+
+if(darkMode === 'enabled'){
+	turnDarkMode();
+}
+
+toggleBtn.addEventListener('click', () => {
+	darkMode = localStorage.getItem('darkMode');
+	if(darkMode !== 'enabled'){
+		turnDarkMode();
+	}
+	else{
+		turnLightMode();
+	}
+})
