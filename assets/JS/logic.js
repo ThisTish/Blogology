88b5d360b58de6c5a0ded8e328cd7724d944f1cb
tishthegoat <tishthegoat@gmail.com>
2024-03-28 23:01:42 -0600

    light/dark toggle working, fonts updated

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -1,43 +1,53 @@
 // todo light/dark toggle
 
-let darkMode = localStorage.getItem('darkMode');
+// const darkMode = localStorage.getItem('darkMode');
+// console.log(darkMode);
 const toggleBtn = document.querySelector('#toggleBtn');
 const image = document.querySelector('#lightDarkImg');
 
 
 // todo walk through and match up with html & css after styling.
 
-const turnDark = ()=> {
-	document.documentElement.classList.add('darkMode');
-	localStorage.setItem('darkMode','dark');
-	image.src = "assets/Images/darkIcons.png";
-}
 
-const turnLight = ()=>{
-	document.documentElement.classList.remove('darkMode');
-	localStorage.setItem('darkMode', 'light');
-	image.src = "assets/images/icons.jpg";
-}
-
-if(darkMode === 'dark'){
-	turnDark();
-}
-else{
-	turnLight();
-}
 
 toggleBtn.addEventListener('click', ()=>{
-	darkMode = localStorage.getItem('darkMode');
-	if(darkMode !== 'dark'){
-		turnDark();
+	// const darkMode = localStorage.getItem('darkMode');
+	const darkModeOn = document.documentElement.classList.toggle('darkMode')
+	if(darkModeOn){
+		console.log('dark')
+		image.src = "assets/Images/darkIcons.png";
 	}
 	else{
-		turnLight();
+		console.log('light')
+		image.src = 'assets/images/icons.jpg'
 	}
+})
+	// const turnDark = ()=> {
+	// 	document.body.classList.add('darkMode');
+	// 	localStorage.setItem('darkMode','dark');
+		// image.src = "assets/Images/darkIcons.png";
+	// }
+	// const turnLight = ()=>{
+	// 	document.body.classList.remove('darkMode');
+	// 	localStorage.setItem('darkMode', 'light');
+	// 	// image.src = "assets/images/icons.jpg";
+	// }
+	// if(darkMode !== 'dark'){
+	// 	turnDark();
+	// }
+	// else{
+	// 	turnLight();
+	// }
 	
-});
+	// if(darkMode === 'dark'){
+	// 	turnDark();
+	// }
+	// else{
+	// 	turnLight();
+	// }
+// });
 
-document.documentElement.classList.toggle('darkmode')
+// document.documentElement.classList.toggle('darkmode')
 // const turnDarkMode = () =>{
 
 // document.documentelement.classList.add('darkMode');
@@ -66,4 +76,3 @@ document.documentElement.classList.toggle('darkmode')
 // 	else{
 // 		turnLightMode();
 // 	}
-// })
